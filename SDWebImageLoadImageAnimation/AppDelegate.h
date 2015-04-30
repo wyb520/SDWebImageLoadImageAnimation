650c16f6f0ec19f7768879aefefdb3ddb57fa59e
YouXianMing <705786299@qq.com>
2015-04-30 18:58:39 +0800

    Initial Commit

---
+++ SDWebImageLoadImageAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDWebImageLoadImageAnimation
+//
+//  Created by YouXianMing on 15/4/30.
+//  Copyright (c) 2015年 YouXianMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDWebImageLoadImageAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDWebImageLoadImageAnimation
+//
+//  Created by YouXianMing on 15/4/30.
+//  Copyright (c) 2015年 YouXianMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
